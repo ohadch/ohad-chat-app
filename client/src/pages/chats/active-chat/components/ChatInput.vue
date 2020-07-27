0b0c869153c647d6c3deddb8bb380bd4add573cf
ohadch <!Motsa14od>
2020-07-27 23:09:11 +0300

    Styles the active chat's layout

--- client/src/pages/chats/active-chat/components/ChatInput.vue
+++ client/src/pages/chats/active-chat/components/ChatInput.vue
@@ -1,13 +1,18 @@
 <template>
-    <div style="display: inline-flex; padding: 0 10px 0 0; margin-bottom: -25px;">
+    <v-form style="display: inline-flex; padding: 0 10px 0 0; margin-bottom: -25px;" @submit.prevent="onMessageSent">
         <v-text-field outlined style="padding: 10px;"/>
         <v-btn class="primary" style="margin: 9px 5px 0 0; height: 57px;">Send</v-btn>
-    </div>
+    </v-form>
 </template>
 
 <script>
     export default {
-        name: "ChatInput"
+        name: "ChatInput",
+        methods: {
+            onMessageSent() {
+                alert("Not implemented")
+            }
+        }
     }
 </script>
 
