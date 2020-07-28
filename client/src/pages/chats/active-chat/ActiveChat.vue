89d64ef7ec518f507401b6b1c1019d26dba136cc
ohadch <!Motsa14od>
2020-07-28 22:53:48 +0300

    Separates ConversationToolbar.vue to a dedicated component

--- client/src/pages/chats/active-chat/ActiveChat.vue
+++ client/src/pages/chats/active-chat/ActiveChat.vue
@@ -1,21 +1,7 @@
 <template>
     <v-layout column justify-space-between>
         <v-flex v-if="conversation">
-            <v-toolbar flat color="#E6E6E6" height="64px">
-                <v-toolbar-title>
-                    <v-list-item style="padding-left: 0;">
-                        <v-list-item-avatar>
-                            <v-img :src="contact.avatar"></v-img>
-                        </v-list-item-avatar>
-
-                        <v-list-item-content>
-                            <v-list-item-title v-html="contact.nickname"></v-list-item-title>
-                            <v-list-item-subtitle
-                                    v-html="'Online or Offline'"></v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-toolbar-title>
-            </v-toolbar>
+            <ConversationToolbar :contact="contact" />
             <v-flex ref="messagesContainer"
                     style="max-height: 80vh; padding: 10px; overflow-y: scroll; scroll-snap-type: y mandatory; overscroll-behavior-y: contain;">
                 <MessageBubble v-for="message in messages" :key="message.id" :message="message"/>
@@ -32,10 +18,12 @@
     import {mapState} from "vuex";
     import ChatInput from "./components/ChatInput";
     import MessageBubble from "./components/MessageBubble";
+    import ConversationToolbar from "./components/ConversationToolbar";
 
     export default {
         name: "ActiveChat",
         components: {
+            ConversationToolbar,
             MessageBubble,
             ChatInput
         },
---
+++ client/src/pages/chats/active-chat/components/ConversationToolbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-toolbar flat color="#E6E6E6" height="64px">
+        <v-toolbar-title>
+            <v-list-item style="padding-left: 0;">
+                <v-list-item-avatar>
+                    <v-img :src="contact.avatar"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                    <v-list-item-title v-html="contact.nickname"></v-list-item-title>
+                    <v-list-item-subtitle
+                            v-html="'Online or Offline'"></v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "ConversationToolbar",
+        props: {
+            contact: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
