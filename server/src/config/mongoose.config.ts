92137d29ddc1b4672674e910e01d5f733ddb7ea7
ohadch <!Motsa14od>
2020-07-30 22:43:08 +0300

    Adds dotenv and consts.config.ts
    Adds dotenv, creates consts.config.ts that resolves the env variables and refactors mongoose.config.ts to take them from the config

--- server/src/config/mongoose.config.ts
+++ server/src/config/mongoose.config.ts
@@ -1,13 +1,14 @@
 import * as mongoose from "mongoose";
+import {MONGO_PASSWORD, MONGO_URI, MONGO_USER} from "./consts.config";
 
-mongoose.connect('mongodb://localhost:27017/', {
+mongoose.connect(MONGO_URI, {
     auth: {
-        user:'root',
-        password:'example'
+        user: MONGO_USER,
+        password: MONGO_PASSWORD
     },
     authSource:"admin",
     useUnifiedTopology: true,
     useNewUrlParser: true
-});
+}).then();
 
 module.exports = mongoose;
